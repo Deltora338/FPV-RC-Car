aafffaa6df5b77d810ebeaab4598076b93f8c018
Deltora338 <Leo.QT.Devlin@gmail.com>
2026-05-27 13:01:57 +1200

    update blink

--- thonny/blink_light.py
+++ thonny/blink_light.py
@@ -1,28 +1,19 @@
 from machine import Pin
 import time
 
-# Built-in LED on most Pico boards is pin 25 (Pico 2 uses Pin 25 as well, Pico W uses 'LED')
+# built in LED
 led = Pin(25, Pin.OUT)
 
-powerPin = Pin(15, Pin.OUT)
-
-outputPin = Pin(14, Pin.OUT)
-inputPin = Pin(16, Pin.IN)
 
 try:
-    led.value(1)
-    outputPin.value(1)
+    led.toggle()
 
     while True:
-        if not inputPin:
-            powerPin.value(1)
-        else:
-            powerPin.value(0)
-
-        time.sleep(0.01)
+        led.toggle()
+        time.sleep(1)
         
 finally:
     led.value(0)
-    print("exited")
+    print("Complete")
     
     
