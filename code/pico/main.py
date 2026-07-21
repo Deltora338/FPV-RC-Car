b368007bfe00d2c7836d7e960169b13af1fdd954
Deltora338 <Leo.QT.Devlin@gmail.com>
2026-07-21 19:47:28 +1200

    Update main.py

--- code/pico/main.py
+++ code/pico/main.py
@@ -1,37 +1,36 @@
 from machine import Pin, PWM, UART
-
-esc = PWM(Pin(14, Pin.OUT, Pin.PULL_DOWN))
-esc.freq(50)
-esc.duty_u16(4915)
-
 import time
 
-servo = PWM(Pin(15))
+servo = PWM(Pin(15, Pin.OUT, Pin.PULL_DOWN))
 servo.freq(50)
 
-uart0 = UART(0, baudrate=420000, rx=Pin(1), tx=Pin(0))
+esc = PWM(Pin(14, Pin.OUT, Pin.PULL_DOWN))  # pull down resistor to eliminate noise on the line
+esc.freq(50)  # standard freq
+esc.duty_u16(4915)  # neutral duty cycle
+
+uart0 = UART(0, baudrate=420000, rx=Pin(1), tx=Pin(0))  # control elrs
 
 led = Pin(25, Pin.OUT)
-led.value(0)
+led.value(1)
 
-def steering(angle, servo):
+def steering(angle, select_servo):
     if angle < 0:
         angle = 0
     elif angle > 180:
         angle = 180
         
-    duty = int(1638 + (angle / 180) * (8192 - 1638))
-    servo.duty_u16(duty)
+    duty = int(1637 + (angle / 180) * (8192 - 1638))
+    select_servo.duty_u16(duty)
     
-def throttle(raw_value, esc_pwm):
+def throttle(raw_value, esc_):
     if raw_value < 174:
         raw_value = 174
     elif raw_value > 1811:
         raw_value = 1811
         
-    # Map 174 -> 1811 (joystick) to 3276 -> 6553 (1ms to 2ms duty cycle)
-    duty = int(3276 + ((raw_value - 174) / (1811 - 174)) * (6553 - 3276))
-    esc_pwm.duty_u16(duty)
+    # map joystick accross range 1811 - 174, to 3276 - 6553 (1ms to 2ms PWM signal)
+    duty = int(3276 + ((raw_value - 174) / (1637)) * (6553 - 3276))
+    esc_.duty_u16(duty)
 
 def decode(payload):
     data = payload[1:]
@@ -63,7 +62,8 @@ def decode_signal_data(payload):
     }
 
 def crsf_crc8(data: bytes) -> int:
-    """calculates the CRSF checksum byte"""
+    """calculates the CRSF checksum byte.
+    This code was taken from the CRSF protocol documentation."""
     crc = 0
     for byte in data:
         crc ^= byte
@@ -72,7 +72,7 @@ def crsf_crc8(data: bytes) -> int:
                 crc = (crc << 1) ^ 0xD5
             else:
                 crc <<= 1
-        crc &= 0xFF  # Ensure it stays an 8-bit integer
+        crc &= 0xFF
     return crc
 
 def read_control(lss, lsq):
@@ -85,33 +85,33 @@ def read_control(lss, lsq):
         data_length = len(raw_data)
         
         i = 0
-        while i < (data_length - 4):  # Needs to be at least long enough for a header + lengthte
-            # 1. Look for the actual Sync Address (0xC8)
+        while i < (data_length - 4):  # needs to be at least long enough for a header + lengthte
+            # check for crsf header
             if raw_data[i] == 0xC8:
                 length = raw_data[i+1]
                 
-                # Make sure we have the full packet in our buffer
+                # check length is valid
                 if i + 2 + length <= data_length:
-                    # Extract the section that the CRC is calculated on (Type + Payload)
-                    # Length includes the CRC byte at the end, so we omit that last byte
+                    # take the part that crc is calculated on
+                    # and the crc that is transmitted with the packet
                     crc_data = raw_data[i+2 : i+1+length] 
                     expected_crc = raw_data[i+1+length]
                     
-                    # 2. Calculate and Validate the CRC
+                    # calculate and compare the crc
                     if crsf_crc8(crc_data) == expected_crc:
-                        # Success! The packet is 100% valid.
                         packet_type = raw_data[i+2]
-                        payload = raw_data[i+2 : i+1+length] # Type + Payload
+                        payload = raw_data[i+2 : i+1+length] # type + Payload
                         
-                        if packet_type == 0x16:  # RC Channels Frame
+                        if packet_type == 0x16:  # control frame
                             channels = decode(payload)
                             
-                        elif packet_type == 0x14: # Link Stats Frame
+                        elif packet_type == 0x1E: # link stats frame
                             stats = decode_signal_data(payload)
                             last_signal_strength = stats['rssi1']
                             last_signal_quality = stats['lq']
                             
                     else:
+                        # crc mismatch, ignore packet
                         pass
                     
                     i += 2 + length
@@ -125,6 +125,10 @@ def read_control(lss, lsq):
                 'raw_channels': channels,
                 'last_signal_strength': last_signal_strength,
                 'last_signal_quality': last_signal_quality,
+                "switch1": channels[4],
+                "switch2": channels[5],
+                "switch3": channels[6],
+                "switch4": channels[7],
             }
             
     return None
@@ -137,8 +141,6 @@ controller_data = {
     'last_signal_quality': 0
 }
 
-print
-led.value(1)
 esc.duty_u16(4915)
 time.sleep(5)
 led.value(0)
@@ -152,21 +154,15 @@ while True:
         raw_steering = controller_data['steering']
         
         steering_angle = round((raw_steering - 174) * (55 - 125) / (1811 - 174) + 125)
-        steering(steering_angle, servo)
+        steering((180 - steering_angle), servo) # servo is mounted upside down
         
         raw_throttle = controller_data['throttle']
         throttle(raw_throttle, esc)
         
-        # Print Debug info
+        # print Debug info
         ch = controller_data['raw_channels']
-        print(f'Throttle: {ch[2]}')
-        print(f'Steering {abs(ch[0] - 992)} {"left" if (ch[0] - 992) <= 0 else "right"}')
-        print(f'Signal: {controller_data["last_signal_strength"]}dBm, {controller_data["last_signal_quality"]}%')
+        # print(f'Throttle: {ch[2]}')
+        # print(f'Steering {abs(ch[0] - 992)} {"left" if (ch[0] - 992) <= 0 else "right"}')
+        # print(f'Signal: {controller_data["last_signal_strength"]}dBm, {controller_data["last_signal_quality"]}%')
     
     time.sleep(0.01)
-
-
-
-
-
-
