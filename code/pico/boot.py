19a05094377dbe9fa166638b65189ca952c070f4
Deltora338 <Leo.QT.Devlin@gmail.com>
2026-07-30 13:14:02 +1200

    Update boot.py

--- code/pico/boot.py
+++ code/pico/boot.py
@@ -1,13 +1,16 @@
-from machine import Pin, PWM, UART
-
 '''The ESC needs to recieve a 2 second neutral throttle pulse
 almost immediatly ofter powerup to arm, or else it will lock itself out.
 This is why the esc pin is defined and used before importing time,
 otherwise we miss the window'''
-esc = PWM(Pin(14, Pin.OUT, Pin.PULL_DOWN))  # pull down resistor to eliminate noise on the line
+
+from machine import Pin, PWM
+import time
+from config import boot_led
+
+esc = PWM(Pin(14, Pin.OUT, Pin.PULL_DOWN))  # pull down resistor to eliminate electrical noise on the line
 esc.freq(50)  # standard freq
 esc.duty_u16(4915)  # neutral duty cycle
 
-import time
-time.sleep(5)
+boot_led.value(1)
 
+time.sleep(5)
